4c3d42135c25885049788db822c50abe7e6b0695
Gabriel Francelino <gabriel-francelino>
2023-02-11 14:22:24 -0300

    Consultas básicas e aninhadas adicionadas

---
+++ scripts/consultas_basicas_aninhadas.sql
@@ -0,0 +1,36 @@
+USE trabalhoBD;
+-- consultas básicas --------------------------------------------------------------------------------------------------------------------------
+-- Selecionar todos os pacientes da tabela Pacientes:
+SELECT * FROM Pacientes;
+-- Selecionar o nome e especialidade dos médicos da tabela Médicos:
+SELECT nome, especialidade FROM Medicos;
+-- Selecionar todas as consultas agendadas com o ID do paciente e do médico:
+SELECT paciente_id, medico_id, data_hora, descricao FROM Consultas;
+-- Selecionar o nome do paciente e a data da receita a partir do ID do paciente na tabela Receitas:
+SELECT nome, data FROM Receitas INNER JOIN Pacientes ON Receitas.paciente_id = Pacientes.id;
+-- Selecionar o nome do médico, data da consulta e descrição a partir do ID do médico na tabela Consultas:
+SELECT nome, data_hora, descricao FROM Consultas INNER JOIN Medicos ON Consultas.medico_id = Medicos.id;
+-- Selecionar o nome do paciente, data do exame e descrição a partir do ID do paciente na tabela Exames:
+SELECT nome, data, descricao FROM Exames INNER JOIN Pacientes ON Exames.paciente_id = Pacientes.id;
+-- Selecionar o nome do médico, data do exame e resultados a partir do ID do médico na tabela Exames:
+SELECT nome, data, resultados FROM Exames INNER JOIN Medicos ON Exames.medico_id = Medicos.id;
+-- Selecionar o nome do paciente, especialidade do médico e horário da consulta a partir do ID do paciente e do médico na tabela Consultas:
+SELECT p.nome, especialidade, data_hora FROM Consultas c
+	INNER JOIN Pacientes p ON c.paciente_id = p.id
+	INNER JOIN Medicos m ON c.medico_id = m.id;
+-- Selecionar o nome do paciente, nome do médico e data da receita a partir do ID do paciente e do médico na tabela Receitas:
+SELECT p.nome, data FROM Receitas r
+	INNER JOIN Pacientes p ON r.paciente_id = p.id
+	INNER JOIN Medicos m ON r.medico_id = m.id;
+-- Selecionar na tabela receitas todos os medicamentos que possuem 500mg .
+SELECT medicamentos FROM Receitas WHERE medicamentos like '%500mg';
+
+-- consultas aninhadas
+
+-- Selecione o nome do paciente e os medicamentos passados a ele, onde os resultados dos exames não estão normais.
+SELECT nome, medicamentos FROM Pacientes p JOIN Receitas r ON p.id = r.paciente_id
+	WHERE p.id IN (SELECT paciente_id FROM Exames WHERE resultados <> 'Tudo normal');
+
+-- Selecione o nome do paciente, onde os resultados dos exames estão normais.
+SELECT nome FROM Pacientes p JOIN Receitas r ON p.id = r.paciente_id
+	WHERE p.id NOT IN (SELECT paciente_id FROM Exames WHERE resultados <> 'Tudo normal');
